e3584422644870b447eec9858078899418ea0f2b
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2012-01-15 09:37:57 +0400

    fix: setting default user is_watcher flag to false

--- db/migrate/20120115050344_add_watcher_columns_to_users.rb
+++ db/migrate/20120115050344_add_watcher_columns_to_users.rb
@@ -2,7 +2,7 @@ class AddWatcherColumnsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :watcher_status, :string
     add_column :users, :organization_id, :integer
-    add_column :users, :is_watcher, :boolean
+    add_column :users, :is_watcher, :boolean, :default => false
 
     add_index :users, :organization_id
     add_index :users, [:is_watcher, :watcher_status]
