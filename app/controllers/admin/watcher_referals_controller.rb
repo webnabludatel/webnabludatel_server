2a8be3d543239a135e5217e9d0716df912bbc6bc
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2012-01-15 09:34:49 +0400

    fixed spelling

--- app/controllers/admin/watcher_referals_controller.rb
+++ app/controllers/admin/watcher_referals_controller.rb
@@ -2,29 +2,29 @@ class Admin::WatcherReferalsController < Admin::Base
   before_filter :find_referal, :only => [:approve, :reject, :problem]
 
   def moderate
-    @refferal = WatcherReferal.not_done.first
+    @referal = WatcherReferal.not_done.first
   end
 
   def approve
-    @refferal.approve! params[:watcher_referal][:comment]
+    @referal.approve! params[:watcher_referal][:comment]
 
     redirect_to moderate_admin_watcher_referals_path
   end
 
   def reject
-    @refferal.reject! params[:watcher_referal][:comment]
+    @referal.reject! params[:watcher_referal][:comment]
 
     redirect_to moderate_admin_watcher_referals_path
   end
   
   def problem
-    @refferal.problem! params[:watcher_referal][:comment]
+    @referal.problem! params[:watcher_referal][:comment]
 
     redirect_to  moderate_admin_watcher_referals_path
   end
 
   protected
     def find_referal
-      @refferal = WatcherReferal.find params[:id]
+      @reffral = WatcherReferal.find params[:id]
     end
 end
