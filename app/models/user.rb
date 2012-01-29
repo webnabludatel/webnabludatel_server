d6fe2955fbc1a7bb5ba039147713f458c55cf9aa
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2012-01-29 19:11:15 +0400

    using new style hashes

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   has_many :device_messages, dependent: :destroy
   has_one :referal, class_name: "WatcherReferal", dependent: :destroy
   has_many :user_locations, dependent: :destroy
-  has_many :comissions, :through => :user_locations
+  has_many :comissions, through: :user_locations
   has_many :messages, dependent: :destroy
 
   WATCHER_STATUSES = %W(pending approved rejected problem blocked none)
