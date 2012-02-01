49ad491ac4d7fd4b0f81f8db3e67170008108373
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2012-02-02 03:28:56 +0400

    fixed typo

--- app/models/watcher_report.rb
+++ app/models/watcher_report.rb
@@ -3,5 +3,5 @@ class WatcherReport < ActiveRecord::Base
   belongs_to :comission
   belongs_to :device_message
 
-  attr_accessible :key, :value, :recorted_at
+  attr_accessible :key, :value, :recorded_at
 end
