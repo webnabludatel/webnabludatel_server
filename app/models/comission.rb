bcd3c48d4afc797ca88afd6667ca5e3aba33de42
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2012-02-02 03:29:04 +0400

    using new style hashes

--- app/models/comission.rb
+++ app/models/comission.rb
@@ -1,8 +1,8 @@
 # encoding: utf-8
 
 class Comission < ActiveRecord::Base
-  has_many :user_locations, :dependent => :destroy
-  has_many :users, :through => :user_locations
+  has_many :user_locations, dependent: :destroy
+  has_many :users, through: :user_locations
   has_many :watcher_reports, dependent: :destroy
 
   # TODO: add validations
