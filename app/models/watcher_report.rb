6537882f69a08e983c5d09c01a8e79701108a332
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2012-02-02 04:45:30 +0400

    watcher report statuses

--- app/models/watcher_report.rb
+++ app/models/watcher_report.rb
@@ -3,5 +3,21 @@ class WatcherReport < ActiveRecord::Base
   belongs_to :comission
   belongs_to :device_message
 
+  validates :key, presence: true
+  validates :value, presence: true
+
+  STATUSES = %W(pending approved rejected problem training manual_rejected manual_suspicious location_unknown check_location location_not_approved no_location)
+
+  STATUSES.each do |status|
+    class_eval <<-EOF
+    scope :#{status}, where(status: :#{status})
+
+    EOF
+  end
+
   attr_accessible :key, :value, :recorded_at
+
+  def status
+    ActiveSupport::StringInquirer.new("#{read_attribute(:status)}")
+  end
 end
