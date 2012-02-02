3eae3fe7826b3896144b0d650c69e0796568894c
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2012-02-02 04:53:03 +0400

    creating watcher report for post device message

--- app/models/device_message.rb
+++ app/models/device_message.rb
@@ -3,9 +3,24 @@
 class DeviceMessage < ActiveRecord::Base
   belongs_to :user
 
-  has_many :watcher_reports, dependent: :destroy
+  has_one :watcher_report, dependent: :destroy
 
   serialize :message
 
+  after_create :create_watcher_report, if: Proc.new{ self.message["MSG_TYPE"] == "post" }
+
   attr_accessible :message
+
+  private
+    def create_watcher_report
+      watcher_report = self.watcher_report.new recorded_at: Time.at(self.message["TIMESTAMP"].to_i)
+      watcher_report.user = self.user
+
+      watcher_report.comission = self.user.current_comission
+
+      watcher_report.key = self.message["PAYLOAD"] && self.message["PAYLOAD"].keys.first
+      watcher_report.value = self.message["PAYLOAD"] && self.message["PAYLOAD"][watcher_report.key]
+
+      watcher_report.save
+    end
 end
