d94d6e4d6c807e13a968f22e023fb891b13f28e3
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2012-02-02 05:30:35 +0400

    watcher report status calculator

---
+++ app/models/watcher_report/status_calculator.rb
@@ -0,0 +1,16 @@
+module WatcherReport::StatusCalculator
+
+  def calculate(current_status, location_status, watcher_status)
+    return current_status if %W(manual_approved manual_rejected manual_suspicious).include? current_status
+    return nil unless watcher_status
+    return watcher_status if watcher_status == "pending"
+
+    if watcher_status == "approved"
+      return "no_location" unless location_status
+      return location_status == "approved" ? "approved" : "location_not_approved"
+    end
+
+    watcher_status
+  end
+
+end
