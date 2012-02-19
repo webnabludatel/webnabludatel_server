ed6cfa8ccb0d82e50902efbb729f174b5d1ee393
Sergey Nebolsin <nebolsin@gmail.com>
2012-02-19 20:41:32 +0400

    Use 'unicorn' instead of 'unicorn_rails' since it's the preferred way to start Unicorn for Rails 3 applications

--- config/deploy/unicorn.rb
+++ config/deploy/unicorn.rb
@@ -4,12 +4,12 @@ set :unicorn_pid, "#{shared_path}/pids/unicorn.pid"
 namespace :deploy do
   desc "Restarts Unicorn server with zero-downtime. If Unicorn server is running, sends USR2 signal to the master process, otherwise starts up the server as usual."
   task :restart, :roles => :app do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D; fi"
+    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
   end
 
   desc "Starts up the Unicorn server."
   task :start, :roles => :app do
-    run "bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D"
+    run "bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
   end
 
   desc "Stops the Unicorn server. Preforms graceful shutdown using QUIT signal."
