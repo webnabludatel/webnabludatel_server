6fb007b8ba670ab5b3a986e692f32238f412ba1e
Sergey Nebolsin <nebolsin@gmail.com>
2012-02-20 03:52:14 +0400

    Disable email on the production since we don't have configured MTA on the server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Watcher::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = false
 
   # Enable threaded mode
   # config.threadsafe!
