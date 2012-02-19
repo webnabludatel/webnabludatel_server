574e5d3e004b87611defbd0ba9c87dd72a1b6db1
Sergey Nebolsin <nebolsin@gmail.com>
2012-02-20 03:35:52 +0400

    Bundle update + unicorn config cleanup

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,7 +7,7 @@ err_log    = "#{rails_root}/log/unicorn_error.log"
 old_pid    = pid_file + '.oldbin'
 
 timeout 30
-worker_processes 4 # Здесь тоже в зависимости от нагрузки, погодных условий и текущей фазы луны
+worker_processes 2
 listen socket_file, :backlog => 1024
 pid pid_file
 stderr_path err_log
@@ -15,16 +15,15 @@ stdout_path log_file
 
 preload_app true # Мастер процесс загружает приложение, перед тем, как плодить рабочие процессы.
 
-GC.copy_on_write_friendly = true if GC.respond_to?(:copy_on_write_friendly=) # Решительно не уверен, что значит эта строка, но я решил ее оставить.
-
 before_exec do |server|
   ENV["BUNDLE_GEMFILE"] = "#{rails_root}/Gemfile"
 end
 
 before_fork do |server, worker|
   # Перед тем, как создать первый рабочий процесс, мастер отсоединяется от базы.
-  defined?(ActiveRecord::Base) and
-  ActiveRecord::Base.connection.disconnect!
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.connection.disconnect!
+  end
 
   # Ниже идет магия, связанная с 0 downtime deploy.
   if File.exists?(old_pid) && server.pid != old_pid
@@ -38,6 +37,7 @@ end
 
 after_fork do |server, worker|
   # После того как рабочий процесс создан, он устанавливает соединение с базой.
-  defined?(ActiveRecord::Base) and
-  ActiveRecord::Base.establish_connection
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
 end
