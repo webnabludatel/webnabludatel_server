9bafb4132414f82072826e86c35a493c48f90412
Sergey Nebolsin <nebolsin@gmail.com>
2012-02-20 14:32:48 +0400

    Strange commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,24 +3,26 @@ require 'rvm/capistrano'
 require 'bundler/capistrano'
 require 'airbrake/capistrano'
 
-server "176.34.112.34", :app, :web, :db, :primary => true
+server '176.34.112.34', :app, :web, :db, :primary => true
 
-set :application, "webnabludatel.ru"
+set :application, 'webnabludatel.ru'
 
 set :deploy_to, "/server/www/#{application}/main/deploy"
 
 set :scm, :git
-set :repository, "git://github.com/webnabludatel/webnabludatel_server.git"
-set :branch, "production"
+set :repository, 'git://github.com/webnabludatel/webnabludatel_server.git'
+set :branch, 'production'
 set :deploy_via, :remote_cache
 
-set :user, "www-data"
+set :user, 'www-data'
 set :use_sudo, false
 set :ssh_options, forward_agent: true
 
-set :rvm_ruby_string, '1.9.3' # Это указание на то, какой Ruby интерпретатор мы будем использовать.
+set :rvm_ruby_string, '1.9.3@webnabludatel'
 set :rvm_type, :system
 
 set :normalize_asset_timestamps, false # Don't need in rails 3
 
-after "deploy", "deploy:cleanup" # keeps only last 5 releases
+depend :remote, :gem, 'bundler', '>=1.0.21'
+
+after 'deploy', 'deploy:cleanup' # keeps only last 5 releases
