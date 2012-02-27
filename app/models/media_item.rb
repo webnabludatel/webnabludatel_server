eb5d9bed55cb2ca7e2e7d2ebd421bdff67b01111
Sergey Nebolsin <nebolsin@gmail.com>
2012-02-27 22:22:07 +0400

    [API] Media item user sync

--- app/models/media_item.rb
+++ app/models/media_item.rb
@@ -1,4 +1,12 @@
 class MediaItem < ActiveRecord::Base
   belongs_to :user_message
   belongs_to :user
+
+  before_save :sync_user
+
+  private
+
+  def sync_user
+    self.user ||= user_message.user
+  end
 end
