5c4a39703d9f96cb3e892502de0270cbad83e3d4
Sergey Nebolsin <nebolsin@gmail.com>
2012-02-27 23:38:42 +0400

    More test devices for commission lookup

--- app/controllers/api/v1/commissions_controller.rb
+++ app/controllers/api/v1/commissions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::CommissionsController < Api::V1::BaseController
   def lookup
-    if params['device_id'] == '0fd7259e241237a685369b26e0abb5cbb9e68722'
+    if %w(0fd7259e241237a685369b26e0abb5cbb9e68722 test_android 358071040269531).include? params['device_id']
       render_result polling_place_region: 77, polling_place_id: 123, polling_place_kind: 'uik'
     else
       render_error "Not Implemented"
