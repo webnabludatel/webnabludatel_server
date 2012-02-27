4fbc0fbed0962ddd87d5cbcdc3571790950a768c
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2012-02-27 07:11:44 +0400

    processing user message after saving

--- app/models/user_message.rb
+++ app/models/user_message.rb
@@ -5,26 +5,12 @@ class UserMessage < ActiveRecord::Base
 
   belongs_to :user_location
 
-  #has_one :watcher_report, dependent: :destroy
-  #after_save :set_watcher_report
+  after_save :process
 
   private
-  #def set_watcher_report
-  #  watcher_report = self.watcher_report || WatcherReport.new
-  #  watcher_report.device_message = self
-  #
-  #  watcher_report.recorded_at = Time.at(self.message["timestamp"].to_i)
-  #  watcher_report.user = self.user
-  #
-  #  watcher_report.comission = self.user.current_comission
-  #
-  #  watcher_report.key = self.message["key"]
-  #  watcher_report.value = self.message["value"]
-  #
-  #  plist_item = WatcherAttribute.find_by_name! self.message["key"]
-  #  watcher_report.watcher_attribute = plist_item
-  #
-  #  watcher_report.save!
-  #end
+    def process
+      analyzer = UserMessagesAnalyzer.new self
+      analyzer.process!
+    end
 
 end
