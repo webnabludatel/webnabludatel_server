83fe2e432d7c848ff8b9ab3cc60d8069a242abed
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2012-02-28 05:38:47 +0400

    fixed typo in user location photo

--- app/models/user_location_photo.rb
+++ app/models/user_location_photo.rb
@@ -1,6 +1,6 @@
 class UserLocationPhoto < ActiveRecord::Base
   belongs_to :user_location
-  belongs_to :meida_item
+  belongs_to :media_item
 
   mount_uploader :image, UserLocationImageUploader
 end
