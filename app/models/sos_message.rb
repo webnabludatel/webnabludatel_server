e29456956819f27a04d5e9858b95fe92d572db4a
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2012-02-28 09:28:42 +0400

    fix

--- app/models/sos_message.rb
+++ app/models/sos_message.rb
@@ -3,7 +3,7 @@
 class SosMessage < ActiveRecord::Base
   belongs_to :user
   belongs_to :user_message
-  belongs_to :location, :class_name => "UserLocation"
+  belongs_to :location, :class_name => "UserLocation", foreign_key: :user_location_id
 
   has_many :photos, class_name: "SosMessagePhoto", dependent: :destroy
 end
