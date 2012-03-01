dfa5df6a2fd27427168edba4d14b116a9d5bccb3
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2012-03-01 22:43:58 +0400

    log errors from analyzer jobs

--- lib/analyze_media_job.rb
+++ lib/analyze_media_job.rb
@@ -1,12 +1,13 @@
 class AnalyzeMediaJob < Struct.new(:id)
   def perform
-    media_item = MediaItem.find id
-    analyzer = MediaItemAnalyzer.new media_item
+    @media_item = MediaItem.find id
+    analyzer = MediaItemAnalyzer.new @media_item
 
     analyzer.process!
   end
 
   def error(job, exception)
+    Rails.logger.error "Media Item Analyzer Error: #{@media_item.inspect}"
     Airbrake.notify(exception)
   end
 end
--- lib/analyze_user_message_job.rb
+++ lib/analyze_user_message_job.rb
@@ -1,12 +1,13 @@
 class AnalyzeUserMessageJob < Struct.new(:id)
   def perform
-    user_message = UserMessage.find id
-    analyzer = UserMessagesAnalyzer.new user_message
+    @user_message = UserMessage.find id
+    analyzer = UserMessagesAnalyzer.new @user_message
 
     analyzer.process!
   end
 
   def error(job, exception)
+    Rails.logger.error "User Message Analyzer Error: #{@user_message.inspect}"
     Airbrake.notify(exception)
   end
 end
