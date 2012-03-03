cb5f757d658945837509e3d512075748a1995c63
Sergey Nebolsin <nebolsin@gmail.com>
2012-03-03 17:06:58 +0400

    First cut of staging deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,21 @@
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Для работы rvm
 require 'rvm/capistrano'
-require 'bundler/capistrano'
-require 'airbrake/capistrano'
+require 'capistrano-deploy'
 
-server '176.34.112.34', :app, :web, :db, :primary => true
+use_recipes :multistage, :bundle
+
+set :default_stage, :production
+
+stage :staging do
+  set :rails_env, 'staging'
+  server '176.34.68.134', :app, :web, :db, :primary => true
+end
+
+stage :production do
+  require 'airbrake/capistrano'
+  set :rails_env, 'production'
+  server '176.34.112.34', :app, :web, :db, :primary => true
+end
 
 set :application, 'webnabludatel.ru'
 
@@ -25,4 +37,5 @@ set :normalize_asset_timestamps, false # Don't need in rails 3
 
 depend :remote, :gem, 'bundler', '>=1.0.21'
 
+after 'deploy:update', 'bundle:install'
 after 'deploy', 'deploy:cleanup' # keeps only last 5 releases
