2c64c9ef8ad64132f72efad0e83606439fd7f459
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2012-03-04 05:30:33 +0400

    start 4 dj workers

--- config/deploy/delayed_job.rb
+++ config/deploy/delayed_job.rb
@@ -1,5 +1,7 @@
 require "delayed/recipes"
 
+set :delayed_job_args, "-n 4"
+
 after "deploy:stop",    "delayed_job:stop"
 after "deploy:start",   "delayed_job:start"
 after "deploy:restart", "delayed_job:restart"
