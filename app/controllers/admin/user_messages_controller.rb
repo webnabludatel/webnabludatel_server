a18d65f0a633931c8f0f7602823fb863b24a5655
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2012-03-04 12:49:26 +0400

    fixed sos_messages

--- app/controllers/admin/user_messages_controller.rb
+++ app/controllers/admin/user_messages_controller.rb
@@ -1,6 +1,5 @@
 class Admin::UserMessagesController < Admin::BaseController
   def index
-    @search = UserMessage.search(params[:search])
-    @user_messages = @search.order("id desc").page(params[:page]).per(50)
+    @user_messages = UserMessage.active.order("id DESC").page(params[:page]).per(50)
   end
 end
