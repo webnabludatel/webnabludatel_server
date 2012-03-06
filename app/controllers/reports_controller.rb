a74a6dd0ef0b9c3b9dc6906a5a79f12e14a98fab
Sergey Nebolsin <nebolsin@gmail.com>
2012-03-07 03:54:47 +0400

    [Reports Page] First cut

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,27 @@
 class ReportsController < ApplicationController
   def index
+    @active_users_count = User.active_count
+    @violations_count = WatcherReport.violations.size
+    @regulations_count = WatcherReport.regulations.size
+
+    @violation_photos = WatcherReportPhoto.interesting.limit(10)
+
+    violation_columns = %W(id violation_text).map {|c| "check_list_items.#{c}"}.join(", ")
+    @popular_violations = CheckListItem.
+        joins(:watcher_reports).
+        select("#{violation_columns}, count(watcher_reports.id) as wcnt").
+        where("watcher_reports.is_violation = true").
+        group(violation_columns).
+        order("wcnt desc").
+        limit(10)
+
+    commission_columns = %W(id number kind region_id).map {|c| "commissions.#{c}"}.join(", ")
+    @bad_commissions = Commission.
+        joins(:watcher_reports).
+        select("#{commission_columns}, count(watcher_reports.id) as wcnt").
+        where("watcher_reports.is_violation = true").
+        group(commission_columns).
+        order("wcnt desc").
+        limit(10)
   end
 end
--- app/models/check_list_item.rb
+++ app/models/check_list_item.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 class CheckListItem < ActiveRecord::Base
-  #has_many :watcher_reports, dependent: :destroy
+  has_many :watcher_reports
 
   has_ancestry
 
