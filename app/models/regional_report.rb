228ac6c89fd98ccad7dcbfafb0ab4fa044fd2620
codesnik <aronaxis@gmail.com>
2012-03-20 19:00:47 +0400

    количество наблюдателей в регионе

--- app/models/regional_report.rb
+++ app/models/regional_report.rb
@@ -5,17 +5,19 @@ class RegionalReport
     # ugly but it works!
     violations_by_region_id = WatcherReport.approved.violations.joins(:commission).count(group: 'commissions.region_id' )
     regulations_by_region_id = WatcherReport.approved.regulations.joins(:commission).count(group: 'commissions.region_id' )
+    users_by_region_id = WatcherReport.joins(:commission).count(:user_id, :group => :region_id, :distinct => true )
 
     Region.all.map do |region|
       violations = violations_by_region_id[region.id.to_s]
       regulations = regulations_by_region_id[region.id.to_s]
+      users = users_by_region_id[region.id.to_s]
 
-      new(region: region, violations: violations, regulations: regulations)
+      new(region: region, violations: violations, regulations: regulations, users: users)
 
     end.reject(&:no_reports?)
   end
 
-  attr_accessor :region, :violations, :regulations
+  attr_accessor :region, :violations, :regulations, :users
 
   def initialize(attrs={})
     attrs.each do |key, value|
